670b0948272bb52b620d4b31eaff64d66bfddb97
Charles Pascoe <charles@cpascoe.co.uk>
2017-09-22 11:30:09 +0100

    feat(lib/index): Add overloads to support transforming types

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-export type Validator<T> =  {
+export type Validator<T> = {
   [key in keyof T]: (arg: any) => T[key]
 };
 
@@ -8,22 +8,8 @@ export type Result<T> = {
 }
 
 
-export type Ident<T> = (arg: T) => T;
-
-
 export interface ILength {
   length: number;
-}
-
-
-export function assertThat<T>(name: string, assertion: (arg: any) => T): (arg: any) => T {
-  return (arg: any) => {
-    try {
-      return assertion(arg);
-    } catch (err) {
-      throw new Error(`Assertion failed for ${name}:\n  ${err.message.split('\n').join('\n  ')}`);
-    }
-  };
 }
 
 
@@ -40,6 +26,17 @@ export function validate<T>(arg: any, validator: Validator<T>): Result<T> {
 }
 
 
+export function assertThat<T>(name: string, assertion: (arg: any) => T): (arg: any) => T {
+  return (arg: any) => {
+    try {
+      return assertion(arg);
+    } catch (err) {
+      throw new Error(`Assertion failed for ${name}:\n  ${err.message.split('\n').join('\n  ')}`);
+    }
+  };
+}
+
+
 // ASSERTIONS //
 
 
@@ -52,7 +49,7 @@ export function optional<T>(next: (arg: any) => T): (arg: any) => T | undefined 
 
 
 export function defaultsTo(def: any): (arg: any) => any;
-export function defaultsTo<T>(def: T, next?: (arg: any) => T): (arg: any) => T;
+export function defaultsTo<T>(def: T, next: (arg: any) => T): (arg: any) => T;
 export function defaultsTo(def: any, next?: (arg: any) => any): (arg: any) => any {
   return (arg: any) => {
     if (arg === undefined) return def;
@@ -61,7 +58,9 @@ export function defaultsTo(def: any, next?: (arg: any) => any): (arg: any) => an
 }
 
 
-export function isBoolean(next?: Ident<boolean>): (arg: any) => boolean {
+export function isBoolean(): (arg: any) => boolean;
+export function isBoolean<T=boolean>(next: (arg: boolean) => T): (arg: any) => T;
+export function isBoolean(next?: (arg: boolean) => any): (arg: any) => any {
   return (arg: any) => {
     if (typeof arg !== 'boolean') throw new Error('Not a boolean');
     return next ? next(arg) : arg;
@@ -69,7 +68,9 @@ export function isBoolean(next?: Ident<boolean>): (arg: any) => boolean {
 }
 
 
-export function isNumber(next?: Ident<number>): (arg: any) => number {
+export function isNumber(): (arg: any) => number;
+export function isNumber<T=number>(next: (arg: number) => T): (arg: any) => T;
+export function isNumber(next?: (arg: number) => any): (arg: any) => any {
   return (arg: any) => {
     if (typeof arg !== 'number') throw new Error('Not a number');
     return next ? next(arg) : arg;
@@ -77,7 +78,9 @@ export function isNumber(next?: Ident<number>): (arg: any) => number {
 }
 
 
-export function min(min: number, next?: Ident<number>): Ident<number> {
+export function min(min: number): (arg: number) => number;
+export function min<T=number>(min: number, next: (arg: number) => T): (arg: number) => T;
+export function min(min: number, next?: (arg: number) => any): (arg: number) => any {
   return (arg: number) => {
     if (arg < min) throw new Error(`${arg} is less than ${min}`);
     return next ? next(arg) : arg;
@@ -85,14 +88,19 @@ export function min(min: number, next?: Ident<number>): Ident<number> {
 }
 
 
-export function max(max: number, next?: Ident<number>): Ident<number> {
+export function max(max: number): (arg: number) => number;
+export function max<T=number>(max: number, next: (arg: number) => T): (arg: number) => T;
+export function max(max: number, next?: (arg: number) => any): (arg: number) => any {
   return (arg: number) => {
     if (arg > max) throw new Error(`${arg} is greater than ${max}`);
     return next ? next(arg) : arg;
   };
 }
 
-export function isString(next?: Ident<string>): (arg: any) => string {
+
+export function isString(): (arg: any) => string;
+export function isString<T=string>(next: (arg: string) => T): (arg: any) => T;
+export function isString(next?: (arg: any) => any): (arg: any) => any {
   return (arg: any) => {
     if (typeof arg !== 'string') throw new Error('Not a string');
     return next ? next(arg) : arg;
@@ -100,7 +108,9 @@ export function isString(next?: Ident<string>): (arg: any) => string {
 }
 
 
-export function matches(regex: RegExp, next?: Ident<string>): Ident<string> {
+export function matches(regex: RegExp): (arg: string) => string;
+export function matches<T=string>(regex: RegExp, next: (arg: string) => T): (arg: string) => T;
+export function matches(regex: RegExp, next?: (arg: string) => any): (arg: string) => any {
   return (arg: string) => {
     if (!regex.test(arg)) throw new Error(`Failed regular expression: ${regex.toString()}`);
     return next ? next(arg) : arg;
@@ -108,7 +118,9 @@ export function matches(regex: RegExp, next?: Ident<string>): Ident<string> {
 }
 
 
-export function minLength<T extends ILength>(min: number, next?: Ident<T>): Ident<T> {
+export function minLength<T extends ILength>(min: number): (arg: T) => T;
+export function minLength<T extends ILength,U=T>(min: number, next: (arg: T) => U): (arg: T) => U;
+export function minLength<T extends ILength>(min: number, next?: (arg: T) => any): (arg: T) => any {
   return (arg: T) => {
     if (arg.length < min) throw new Error(`Length ${arg.length} is less than ${min}`);
     return next ? next(arg) : arg;
@@ -116,7 +128,9 @@ export function minLength<T extends ILength>(min: number, next?: Ident<T>): Iden
 }
 
 
-export function maxLength<T extends ILength>(max: number, next?: Ident<T>): Ident<T> {
+export function maxLength<T extends ILength>(max: number): (arg: T) => T;
+export function maxLength<T extends ILength,U=T>(max: number, next: (arg: T) => U): (arg: T) => U;
+export function maxLength<T extends ILength>(max: number, next?: (arg: T) => any): (arg: T) => any {
   return (arg: T) => {
     if (arg.length > max) throw new Error(`Length ${arg.length} is greater than ${max}`);
     return next ? next(arg) : arg;
@@ -124,7 +138,9 @@ export function maxLength<T extends ILength>(max: number, next?: Ident<T>): Iden
 }
 
 
-export function lengthIs<T extends ILength>(length: number, next?: Ident<T>): Ident<T> {
+export function lengthIs<T extends ILength>(length: number): (arg: T) => T;
+export function lengthIs<T extends ILength,U=T>(length: number, next: (arg: T) => U): (arg: T) => U;
+export function lengthIs<T extends ILength>(length: number, next?: (arg: T) => any): (arg: T) => any {
   return (arg: T) => {
     if (arg.length !== length) throw new Error(`Length ${arg.length} is not equal to ${length}`);
     return next ? next(arg) : arg;
@@ -133,8 +149,8 @@ export function lengthIs<T extends ILength>(length: number, next?: Ident<T>): Id
 
 
 export function isArray(): (arg: any) => any[];
-export function isArray<T>(next: (arg: any[]) => T[]): (arg: any) => T[];
-export function isArray(next?: (arg: any[]) => any[]): (arg: any) => any[] {
+export function isArray<T>(next: (arg: any[]) => T): (arg: any) => T;
+export function isArray(next?: (arg: any[]) => any): (arg: any) => any {
   return (arg: any) => {
     if (!(arg instanceof Array)) throw new Error('Not an array');
     return next ? next(arg) : arg;
@@ -156,9 +172,13 @@ export function conformsTo<T>(validator: Validator<T>): (arg: any) => T {
 }
 
 
-export function equals<T>(value: T): (arg: any) => T {
+export function equals<T>(value: T, ...values: T[]): (arg: any) => T {
+  let vals = [value, ...values];
   return (arg: any) => {
-    if (arg !== value) throw new Error(`'${arg}' does not equal '${value}'`);
-    return arg;
+    for (let val of vals) {
+      if (val === arg) return arg;
+    }
+
+    throw new Error(vals.length === 1 ? `'${arg}' does not equal '${vals[0]}'` : `'${arg}' not one of: ${vals.join(', ')}`);
   };
 }
