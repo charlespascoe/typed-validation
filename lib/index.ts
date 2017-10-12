315c23721006434e4bf7d7ce7e30932b3e3c13b3
Charles Pascoe <charles@cpascoe.co.uk>
2017-10-12 10:57:26 +0100

    feat(lib/index): Add extendValidator method
    Fixes #2

--- lib/index.ts
+++ lib/index.ts
@@ -26,6 +26,21 @@ export function validate<T>(arg: any, validator: Validator<T>): Validated<T> {
 }
 
 
+export function extendValidator<T,U>(validator1: Validator<T>, validator2: Validator<U>): Validator<T & U> {
+  let result: any = {};
+
+  for (let key in validator1) {
+    result[key] = validator1[key];
+  }
+
+  for (let key in validator2) {
+    result[key] = validator2[key];
+  }
+
+  return result as Validator<T & U>;
+}
+
+
 export function assertThat<T>(name: string, assertion: (arg: any) => T): (arg: any) => T {
   return (arg: any) => {
     try {
